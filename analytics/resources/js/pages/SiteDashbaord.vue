b89be6727419e5143528e8e5adf9d78751ff13f3
Tarek <tarek@fluentapp.io>
2023-12-11 09:13:07 +0200

    fix filter navbar

--- analytics/resources/js/pages/SiteDashbaord.vue
+++ analytics/resources/js/pages/SiteDashbaord.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="row">
-        <GlobalFilters @update-filter="updateFilter" />
+        <div ref="globalFiltersRef" :class="{ fixed: isFixed }">
+            <GlobalFilters @update-filter="updateFilter" />
+        </div>
         <Visitors />
         <Sources />
         <Pages />
@@ -11,7 +13,7 @@
     </div>
 </template>
 <script setup>
-import { ref, provide } from "vue";
+import { ref, provide, onMounted, onUnmounted } from "vue";
 
 const props = defineProps(["domain", "withNotFound", "withExternal"]);
 const filter = ref({
@@ -22,8 +24,37 @@ const filter = ref({
 });
 provide("filter", filter);
 provide("domain", props.domain);
+const globalFiltersRef = ref(null);
+const isFixed = ref(false);
+
+onMounted(() => {
+    window.addEventListener("scroll", handleScroll);
+});
+
+onUnmounted(() => {
+    window.removeEventListener("scroll", handleScroll);
+});
+const handleScroll = () => {
+    if (globalFiltersRef.value) {
+        isFixed.value = window.scrollY > globalFiltersRef.value.offsetTop;
+    }
+};
 
 const updateFilter = (newFilterType) => {
     filter.value.period = newFilterType;
 };
 </script>
+<style scoped>
+.fixed {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    z-index: 99;
+    padding-left: 7%;
+    padding-right: 7%;
+    background: white;
+    padding-top: 1rem;
+    border-bottom: 1px solid #cecece;
+}
+</style>
